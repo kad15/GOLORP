89557d2d3c0d5efa74d09c6ae2ab371e2512627a
toto <kader.beldjilali@free.fr>
2018-11-26 17:44:09 +0100

    divers

--- PROLOG_dernier_TP/travailleur.pl
+++ PROLOG_dernier_TP/travailleur.pl
@@ -1,7 +1,7 @@
 % avec variables booléennes : Xij = si travailleur i réalise le produit j
 % modélisation : variables V, Domaine D et Contraintes C
 sol(L) :- L=[X11, X12, X13,X14,X21,X22,X23,X24,X31,X32,X33,X34,X41,X42,X43,X44],  
-			fd_domain_bool(L),  /* Domaine boolean */
+			fd_domain_bool(L),  /* Domaine boolean des variables */
 			/*ctr : chaque travailleur n'a qu'un produit */
 			X11+X12+X13+X14 #= 1, 
 			X21+X22+X23+X24 #= 1,
@@ -9,14 +9,18 @@ sol(L) :- L=[X11, X12, X13,X14,X21,X22,X23,X24,X31,X32,X33,X34,X41,X42,X43,X44],
 			X41+X42+X43+X44 #= 1,
 			/* ctr : chaque produit est réalisé par un seul travailleur1*/
 			X11+X21 +X31+X41 #= 1,   
-			X11+X21 +X31+X41 #= 1, 
-			X11+X21 +X31+X41 #= 1,
-			X11+X21 +X31+X41 #= 1,
+			X12+X22 +X32+X42 #= 1, 
+			X13+X23 +X33+X43 #= 1,
+			X14+X24 +X34+X44 #= 1,
 			/*objectif */
-			Profit #= 7 * X11 + 1 * X12 + 3 * X13 + 4 * X14 +8 * X21 + 2 * X22 + 5 * X23 + 1* X24 +4 * X31 + 3 * X32 + 7 * X33 + 2 * X34 +3 * X41 + X42 + 6 * X43 + 3 * X44,
-			fd_maximize(fd_labelling(L), Profit).
+			Profit #= (7 * X11 + 1 * X12 + 3 * X13 + 4 * X14 +
+			8 * X21 + 2 * X22 + 5 * X23 + 1* X24 +
+			4 * X31 + 3 * X32 + 7 * X33 + 2 * X34 +
+			3 * X41 + X42 + 6 * X43 + 3 * X44),
+			fd_maximize(fd_labeling(L), Profit).
 			
 /*
+error(existence_error(procedure,fd_labelling/1),fd_maximize/2)
 fd_minimize(Goal, X) repeatedly calls Goal to find a value that minimizes 
 the variable X. Goal is a Prolog goal that should instantiate X, 
 a common case being the use of fd_labeling/2 (section 9.9.1)
